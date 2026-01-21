81d12a6579b1c273781eabe5115d6d8aa77c291c
Michael Ferreira <77752895+michaeljf07@users.noreply.github.com>
2026-01-21 14:16:46 -0500

    Time: 4 ms (41.12%), Space: 19.4 MB (15.94%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -2,14 +2,17 @@ class Solution:
     def mySqrt(self, x: int) -> int:
         if x == 0:
             return 0
-        elif x == 1:
-            return 1
         
-        search_bounds = range(1, x // 2 + 1)
-
-        for n in search_bounds:
-            if n * n == x:
-                return n
-            elif not (n+1) * (n+1) <= x:
-                return n
+        left = 1
+        right = x
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return right
             
