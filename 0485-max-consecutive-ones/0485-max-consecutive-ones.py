7a0c48ee27c49f3d1c139ba1b4dd976478dc0e95
Michael Ferreira <77752895+michaeljf07@users.noreply.github.com>
2026-01-24 18:24:49 -0500

    Time: 8 ms (91.03%), Space: 22 MB (7.65%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.py
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.py
@@ -6,7 +6,7 @@ class Solution:
         for i in nums:
             if i == 1:
                 cur_count += 1
-                max_count = max(max_count, cur_count)
+                max_count = cur_count if cur_count > max_count else max_count
             else:
                 cur_count = 0
 
