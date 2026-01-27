4ee85d10016942032bbaf3d405f933a5bebab2e8
Michael Ferreira <77752895+michaeljf07@users.noreply.github.com>
2026-01-27 09:26:33 -0500

    Time: 17 ms (77.51%), Space: 20.8 MB (33.25%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.py
+++ 0697-degree-of-an-array/0697-degree-of-an-array.py
@@ -1,8 +1,8 @@
 class Solution:
     def findShortestSubArray(self, nums: list[int]) -> int:
-        first = {}  # tracks first occurence of a number
-        last = {}   # tracks last occurence of a number
-        count = {}  # tracks the number of times each number appears
+        first: dict = {}  # tracks first occurence of a number
+        last: dict = {}   # tracks last occurence of a number
+        count: dict = {}  # tracks the number of times each number appears
         
         for i, num in enumerate(nums):
             if num not in first: 
@@ -11,10 +11,10 @@ class Solution:
             last[num] = i
             count[num] = count.get(num, 0) + 1
             
-        degree = max(count.values())
+        degree: int = max(count.values())
         
         # Find the minimum length among numbers that hit the degree
-        min_len = len(nums)
+        min_len: int = len(nums)
         for x in count:
             if count[x] == degree:
                 min_len = min(min_len, last[x] - first[x] + 1)
