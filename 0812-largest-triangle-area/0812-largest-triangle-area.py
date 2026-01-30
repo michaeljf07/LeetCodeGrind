17ec738bc714960ad751e921809a30f7841b466e
Michael Ferreira <77752895+michaeljf07@users.noreply.github.com>
2026-01-30 11:37:29 -0500

    Time: 35 ms (86.2%), Space: 19.5 MB (31.44%) - LeetHub

--- 0812-largest-triangle-area/0812-largest-triangle-area.py
+++ 0812-largest-triangle-area/0812-largest-triangle-area.py
@@ -5,9 +5,9 @@ class Solution:
 
         for i in range(len_points):
             x1, y1 = points[i]
-            for j in range(len_points):
+            for j in range(i+1, len_points):
                 x2, y2 = points[j]
-                for k in range(len_points):
+                for k in range(j+1, len_points):
                     x3, y3 = points[k]
 
                     area = 0.5 * abs(x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1 - y2))
