495781bbf0fb71fa948588c6495340f997c42bd0
Michael Ferreira <77752895+michaeljf07@users.noreply.github.com>
2026-01-31 11:11:30 -0500

    Time: 51 ms (0.27%), Space: 19.7 MB (12.9%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
@@ -6,15 +6,10 @@
 #         self.right = right
 class Solution:
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
-        if root is None:
+        if not root:
             return 0
-      
-        total_sum = self.sumOfLeftLeaves(root.right)
-      
-        if root.left:
-            if root.left.left is None and root.left.right is None:
-                total_sum += root.left.val
-            else:
-                total_sum += self.sumOfLeftLeaves(root.left)
-      
-        return total_sum
+
+        if root.left and not root.left.left and not root.left.right:
+            return root.left.val + self.sumOfLeftLeaves(root.right)
+
+        return self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
