efa6fb718478708c1bc0852568ed3079b27ddcbd
Michael Ferreira <77752895+michaeljf07@users.noreply.github.com>
2026-02-23 11:58:31 -0500

    Time: 1 ms (76.4%), Space: 19.3 MB (63.74%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -10,18 +10,17 @@ class Solution:
         carry = 0
 
         while carry or l1 or l2:
-            l1_val = l1.val if l1 else 0
-            l2_val = l2.val if l2 else 0
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
 
-            digit_sum = l1_val + l2_val + carry
-            carry = digit_sum // 10
+            total = val1 + val2 + carry
+            carry = total // 10
+            digit = total % 10
 
-            new_node = ListNode(digit_sum % 10)
-            cur.next = new_node
-            cur = new_node
+            cur.next = ListNode(digit)
+            cur = cur.next
 
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
+            l1 = l1.next if l1 else l1
+            l2 = l2.next if l2 else l2
 
         return dummy.next
-        
