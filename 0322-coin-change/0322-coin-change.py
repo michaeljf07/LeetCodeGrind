68391a04f69b37fe68b81bc6a5dff7bd620c203e
Michael Ferreira <77752895+michaeljf07@users.noreply.github.com>
2026-08-15 15:13:57 -0400

    Time: 441 ms (91.07%), Space: 19.6 MB (52.48%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,10 +1,11 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [amount + 1] * (amount + 1)
+        INVALID_DEFAULT = float("inf")
+        dp = [INVALID_DEFAULT] * (amount + 1) # dp[a] = the fewest number of coins to make amount a
         dp[0] = 0
 
-        for a in range(1, amount + 1):
-            for c in coins:
-                if a - c >= 0:
-                    dp[a] = min(dp[a], 1 + dp[a - c])
-        return dp[amount] if dp[amount] != amount + 1 else -1
+        for coin in coins:
+            for a in range(coin, amount + 1):
+                dp[a] = min(dp[a], 1 + dp[a - coin])
+        
+        return dp[amount] if dp[amount] != INVALID_DEFAULT else -1
