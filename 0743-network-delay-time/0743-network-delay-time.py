721846020edc0c0c9c927c95a34059e8d21bb279
Michael Ferreira <77752895+michaeljf07@users.noreply.github.com>
2026-09-13 12:14:38 -0400

    Time: 340 ms (52.48%), Space: 22 MB (8.63%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -2,26 +2,37 @@ from collections import defaultdict
 import heapq
 
 
+"""
+Task: find the shortest path in a directed weighted graph
+- Suited for Dijkstra's algorithm
+
+Steps:
+- Create an adjacency map
+- 
+"""
+
+
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        edges = defaultdict(list)  # source node -> (target node, time)
+        adj_map = defaultdict(list)
         for u, v, time in times:
-            edges[u].append((v, time))
+            adj_map[u].append((v, time))
 
-        min_heap = [(0, k)] # stores (total path, node)
+        min_heap = [(0, k)] # stores (path_so_far, node)
         visited = set()
         time = 0
 
         while min_heap:
-            weight, node = heapq.heappop(min_heap)
+            path_so_far, node = heapq.heappop(min_heap)
             # avoid cycles
             if node in visited:
                 continue
             visited.add(node)
-            time = max(time, weight)
 
-            for nghbr_node, nghbr_weight in edges[node]:
+            time = max(time, path_so_far)
+            
+            for nghbr_node, nghbr_weight in adj_map[node]:
                 if nghbr_node not in visited:
-                    heapq.heappush(min_heap, (weight + nghbr_weight, nghbr_node))
-        
+                    heapq.heappush(min_heap, (time + nghbr_weight, nghbr_node))
+
         return time if len(visited) == n else -1
