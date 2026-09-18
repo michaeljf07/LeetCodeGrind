8bf186e1b1e7a11d9e81a3d77823dfd5bfbd22bb
Michael Ferreira <77752895+michaeljf07@users.noreply.github.com>
2026-09-18 19:23:42 -0400

    Time: 3 ms (60.61%), Space: 21.1 MB (44.77%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        xor = 0
+        res = 0
         for num in nums:
-            xor ^= num
-        
-        return xor
+            # XOR is commutative and associative so it doesn't matter if we XOR adjacent numbers that are not equal
+            res = res ^ num
+        return res 
